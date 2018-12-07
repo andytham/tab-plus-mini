f16e91a4610b744b7da7be4568e387390aae6e24
andy <thamandyy@gmail.com>
2018-12-06 21:04:39 -0500

    loading

--- includes/unloaded-tabs.js
+++ includes/unloaded-tabs.js
@@ -12,18 +12,21 @@ function setTabUnload (tab) {
   tab.removeAttribute("load");
 }
 function onLoad (e) {
+  console.log("window load");
   var tab = e.target;
   // tab.setAttribute("isLoaded","true");
-  console.log("window load", tab);
+
+  console.log(tab);
 }
 function initTabUnload(window) {
   
   setTabUnload(window.gBrowser.selectedTab);
 
   // sessionStore.persistTabAttribute("load");
-
+  console.log("logging window");
+  console.log(window);
   window.gBrowser.tabContainer.addEventListener("TabSelect", setTabLoadOnTabSelect, true);
-  window.addEventListener("load", onLoad, true)
+  window.gBrowser.tabs.addEventListener("load", onLoad, false);
   unload(function() window.gBrowser.tabContainer.removeEventListener("load", setTabLoadOnTabSelect));
   unload(function() window.gBrowser.tabContainer.removeEventListener("TabSelect", setTabLoadOnTabSelect));
 
