fae270349dfa4be615c58e93abb4c77b47af8546
andy <thamandyy@gmail.com>
2018-12-07 16:23:58 -0500

    If logic on close tabs

--- includes/previous-tab.js
+++ includes/previous-tab.js
@@ -1,31 +1,32 @@
 let previousTabIndex = -1;
+let prevTemp = -2;
 let currentTabIndex = 1;
 let win;
 function closeTab(e){
 	let closingTab = e.target;
 	let currentTab = win.selectedTab;
-	console.log(closingTab._tPos, currentTab._tPos);
-	//check if active tab is being closed
-	// if (closingTab._tPos != currentTab._tPos){
-	// 	//check if user has used more than one tab
-	// 	if (previousTabIndex == -1){
-	// 		//check if last tab on list
-	// 		if (currentTabIndex < win.selected ){
-	// 			win.selectedTab = win.tabs[currentTabIndex + 1]
-	// 		} else {
-	// 			win.selectedTab = win.tabs[currentTabIndex - 1]
-	// 		}
-			
-	// 	} else {
-	// 		console.log("end")
-	// 	}
-	// }
+	let prevTab = win.tabs[previousTabIndex]
 
+
+	// check if active tab is being closed
+	if (closingTab._tPos == prevTab._tPos){
+		//check if loaded more than one tab
+		console.log('first condition');
+		if (prevTemp == -1){
+			console.log("no prev tab");
+		} else {
+			win.selectedTab = win.tabs[prevTemp]
+		}
+	} else {
+		console.log("don't need to do anything");
+		// win.selectedTab = win.tabs[previousTabIndex]
+	}
 }
 
 function setCurrentTab(e){
 	let tab = e.target;
 	if (currentTabIndex){
+		prevTemp = previousTabIndex;
 		previousTabIndex = currentTabIndex;
 	}
 	currentTabIndex = tab._tPos;
@@ -37,6 +38,6 @@ function initPreviousTab(window){
 	win = window.gBrowser;
 	window.gBrowser.tabContainer.addEventListener("TabClose", closeTab, true);
 	window.gBrowser.tabContainer.addEventListener("TabSelect", setCurrentTab, true)
-	unload(function() window.gBrowser.tabContainer.removeEventListener("tabClose", closeTab));
-	unload(function() window.gBrowser.tabContainer.removeEventListener("TabSelect", setCurrentTab));
+	unload(function() {window.gBrowser.tabContainer.removeEventListener("tabClose", closeTab)});
+	unload(function() {window.gBrowser.tabContainer.removeEventListener("TabSelect", setCurrentTab)});
 }
