f870ef116042f145cc91a62077df6f9e0b0d94d9
andy <thamandyy@gmail.com>
2018-12-06 20:46:55 -0500

    getting somewhere

--- includes/unloaded-tabs.js
+++ includes/unloaded-tabs.js
@@ -1,21 +1,30 @@
-function setTabReadOnTabLoad (e) {
+function setTabLoadOnTabSelect (e) {
   var tab = e.target;
   setTabLoad(tab);
 }
 
 function setTabLoad (tab) {
   tab.setAttribute("load", "true");
+  console.log("load set");
 }
 
 function setTabUnload (tab) {
   tab.removeAttribute("load");
 }
-
+function onLoad (e) {
+  var tab = e.target;
+  // tab.setAttribute("isLoaded","true");
+  console.log("window load", tab);
+}
 function initTabUnload(window) {
-  setTabRead(window.gBrowser.selectedTab);
+  
+  setTabUnload(window.gBrowser.selectedTab);
 
-  sessionStore.persistTabAttribute("load");
+  // sessionStore.persistTabAttribute("load");
 
-  window.gBrowser.tabContainer.addEventListener("TabSelect", setTabReadOnTabLoad, true);
-  unload(function() window.gBrowser.tabContainer.removeEventListener("TabSelect", setTabReadOnTabLoad));
+  window.gBrowser.tabContainer.addEventListener("TabSelect", setTabLoadOnTabSelect, true);
+  window.addEventListener("load", onLoad, true)
+  unload(function() window.gBrowser.tabContainer.removeEventListener("load", setTabLoadOnTabSelect));
+  unload(function() window.gBrowser.tabContainer.removeEventListener("TabSelect", setTabLoadOnTabSelect));
+
 }
